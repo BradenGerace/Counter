7dcedc776f33119be7dbf848a94f4e67c0f82740
Braden Gerace <braden.gerace@hotmail.com>
2023-05-11 17:10:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ btns.forEach(function (btn) {
         else {
             count = 0;
         }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        else if (count > 0) {
+            value.style.color = "green";
+        }
+        else {
+            value.style.color = "black";
+        }
         value.textContent = count;
     })    
 })
