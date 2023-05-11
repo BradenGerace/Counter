6322255ac4eda773b33a0143e456d97a971218cd
Braden Gerace <braden.gerace@hotmail.com>
2023-05-11 15:35:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 // set initial count
 let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelector(".btn");
+
+btns.forEach(function (item) {
+    console.log(item);
+})
